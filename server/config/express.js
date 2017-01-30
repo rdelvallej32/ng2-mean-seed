952cfeb0fa951e43c61d2cdf8e51a41390661a8e
rdelvallej32 <rdelvallej@gmail.com>
2017-01-29 19:54:36 -0500

    Add compression to express app & add loading progress bar

--- server/config/express.js
+++ server/config/express.js
@@ -5,6 +5,7 @@ import logger from 'morgan';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import errorHandler from 'errorhandler';
+import compression from 'compression';
 import config from './environments';
 
 export default function(app) {
@@ -15,11 +16,14 @@ export default function(app) {
   }
 
   if(env === 'production') {
+    // TODO: need to configure app to work in production environment
   }
 
   // view engine setup
   app.set('views', path.join(config.root, './server/views'));
   app.set('view engine', 'hbs');
+
+  app.use(compression());
 
   // uncomment after placing your favicon in /public
   //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
