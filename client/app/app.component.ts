c926ac5606cf1b88efad63d0582c53837f96d330
rdelvallej32 <rdelvallej@gmail.com>
2017-02-27 19:41:47 -0500

    Change css files to sass files

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,26 +1,19 @@
 import { Component } from '@angular/core';
-import { MdSnackBar } from '@angular/material';
 import * as io from "socket.io-client";
 
 @Component({
     selector: 'main-app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
     socket = null;
 
-    constructor(public snackbar: MdSnackBar) {
+    constructor() {
         this.socket = io('localhost:3000');
         this.socket.on('test', data => {
             console.log(data);
-        });
-    }
-
-    showSnackbar() {
-        this.snackbar.open('Good Job Clicking', 'Close', {
-            duration: 2000
         });
     }
 }
