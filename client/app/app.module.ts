ae4e8d2715ee34acd3d7f46eff629a3c29658440
rdelvallej32 <rdelvallej@gmail.com>
2017-02-27 20:10:40 -0500

    Add bootstrap navbar without routes
    - Remove material from app module for time being
    - Add basic bootstrap navbar
    - Add padding to h1 tag in app component
    
    TODO: Need to add basic sample routing for seed app

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
+
+import { HeaderComponent } from './common/header/header.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent
     ],
     imports: [
-        BrowserModule,
-        MaterialModule.forRoot()
+        BrowserModule
     ],
     bootstrap: [
         AppComponent
