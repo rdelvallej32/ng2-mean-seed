c696dfcc6db0cd3af8c581f7b4fbcb75e02f01b9
rdelvallej32 <rdelvallej@gmail.com>
2017-02-28 19:47:38 -0500

    Import interface and create a Routes constant

---
+++ client/app/common/header/header-routes.ts
@@ -0,0 +1,6 @@
+import { MenuType, RouteInfo } from './header.model';
+
+export const ROUTES: RouteInfo[] = [
+    { path: '', title: 'App Brand Here', menuType: MenuType.BRAND },
+    { path: '', title: 'Sample Route', menuType: MenuType.LEFT }
+]
