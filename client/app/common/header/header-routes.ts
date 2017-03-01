c7cc095725ab0165fb562abdffd44c5bff1d8fd9
rdelvallej32 <rdelvallej@gmail.com>
2017-02-28 20:03:48 -0500

    Add Additional routes in nav

--- client/app/common/header/header-routes.ts
+++ client/app/common/header/header-routes.ts
@@ -2,5 +2,7 @@ import { MenuType, RouteInfo } from './header.model';
 
 export const ROUTES: RouteInfo[] = [
     { path: '', title: 'App Brand Here', menuType: MenuType.BRAND },
-    { path: '', title: 'Sample Route', menuType: MenuType.LEFT }
+    { path: '', title: 'One Route', menuType: MenuType.LEFT },
+    { path: '', title: 'Two Route', menuType: MenuType.LEFT },
+    { path: '', title: 'Three Route', menuType: MenuType.LEFT }
 ]
