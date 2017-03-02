d4a667ae898f603e99316931f1c93cc7bdd96292
rdelvallej32 <rdelvallej@gmail.com>
2017-03-01 19:40:58 -0500

    Create boilerplate for a search-bar component

---
+++ client/app/common/search-bar/search-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+    selector: 'search-bar',
+    templateUrl: './search-bar.component.html',
+    styleUrls: ['./search-bar.component.scss']
+})
+
+export class SearchBarComponent {
+
+}
